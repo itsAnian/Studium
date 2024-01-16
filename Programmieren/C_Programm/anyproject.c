bcc56563e12591e9e8d4b44543f1b0500c4827ad
anse <anian.seidl@redlion.net>
2024-01-16 17:29:12 +0100

    first commit for c, rm Beispiele Web-Engineering

---
+++ Programmieren/C_Programm/anyproject.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef TESTING
+    int main(int argc, char** argv){
+        printf("Hello World!\n");
+        //das ist ein Test
+	//Test zurück
+        return (EXIT_SUCCESS);
+    }
+#endif
